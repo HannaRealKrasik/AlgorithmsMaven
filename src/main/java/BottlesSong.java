350d9f2ac6e8326d8aaa559ff91435690239e634
¨Hanna <hannarealkrasik@gmail.com>
2021-10-21 20:21:11 -0500

    song changes

--- src/main/java/BottlesSong.java
+++ src/main/java/BottlesSong.java
@@ -6,21 +6,12 @@ public class BottlesSong {
         for (int q = 0; q < song.length; q++){
             song[q] = String.valueOf(i) + first + String.valueOf(i) + second + String.valueOf(i - 1) + third;
             i = i -1;
-            if(i == 2)
-                ;
+            if(i == 2);
                 song[q] = String.valueOf(i) + first + String.valueOf(i) + second + String.valueOf(i - 1) + third.replace("bottles", "bottle");
-
             if (i == 1)
-
-
                 song[q] = String.valueOf(i) + first.replace("bottles", "bottle") + String.valueOf(i) + second.replace("bottles", "bottle") + "no more" + third.replace("bottles", "bottle");;
             if(i == 0)
                 song[q] = fourth;
-
-
-
-
-
     }return song;
 }
 }
--- src/test/java/songTest.java
+++ src/test/java/songTest.java
@@ -10,7 +10,9 @@ public class songTest {
         String third = " bottles of beer on the wall.\n" ;
         String fourth = "No more bottles of beer on the wall, no more bottles of beer.\n" +
                 "Go to the store and buy some more, 99 bottles of beer on the wall.";
+
         BottlesSong bottlesSong = new BottlesSong();
+
         String [] array =BottlesSong.Arrays(i,first,second,third,fourth);
         for (int q =0; q<array.length;q++)
         System.out.println(array[q]);
